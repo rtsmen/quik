1de27caff0b3694d6e7265df72875579c6a2c2be
Zabiyakin Vladimir <rtsmen@gmail.com>
2016-02-29 23:55:05 +0300

    лог должен писаться в один файл

--- MOMENT.lua
+++ MOMENT.lua
@@ -1,10 +1,12 @@
-p_classcode="TQBR" --��� ������
-p_seccode="LKOH" --��� �����������
+p_classcode="SPFUT" --��� ������
+p_seccode="SiH6" --��� �����������
 is_run=true
 count=1
+l_file=nil
 function main()
+      l_file=io.open("C:"..tostring(count)..".txt", "w")
       while is_run do
-            sleep(2000)
+            sleep(1000)
       end
 end
 function OnStop(stop_flag)
@@ -12,9 +14,7 @@ function OnStop(stop_flag)
 end
 function OnQuote(class_code, sec_code)
       if class_code==p_classcode and sec_code==p_seccode then
-            l_file=io.open("G:"..tostring(count)..".txt", "w")
             tb=getQuoteLevel2(class_code, sec_code)
-           
             l_file:write("BID:\n")
             for i=1,2,1 do --tb.bid_count
                   l_file:write(tostring(tb.bid[i].price)..";  "..
